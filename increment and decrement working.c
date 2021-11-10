e5ed607e2f6bf684d16475804d8bb1be52d16173
tony <anthonychege599@gmail.com>
2021-11-10 05:17:59 -0800

    Create increment and decrement working.c

---
+++ increment and decrement working.c
@@ -0,0 +1,15 @@
+/*increment and decriment operator working*/
+#include<stdio.h>
+int main()
+
+{
+    int p=13;
+    int q=20;
+    float r=20.55;
+    float s=100.01;
+    printf("p=%d",++p);
+    printf("q=%d",--q);
+    printf("r=%f",++r);
+    printf("s=%f",--s);
+    return 0;
+}
