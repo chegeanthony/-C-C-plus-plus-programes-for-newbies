966019fc00372f013542bf07b6290a247c671820
tony <anthonychege599@gmail.com>
2021-11-10 04:58:14 -0800

    Create local variables.c

---
+++ local variables.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int a=20,b=10;//local variable
+    int sum;
+    int product;
+    int division;
+    sum=a+b;
+    product=a*b;
+    division=a/b;
+    printf("sum=%d",sum);//prints the sum
+    printf("product=%d",product);
+    printf("division=%d",division);
+    return 0;//this terminates the function
+}
