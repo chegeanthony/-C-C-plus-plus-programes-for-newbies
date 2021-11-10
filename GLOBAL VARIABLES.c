7e805f8f048dd386f7914c70a77f31b04e14ba4c
tony <anthonychege599@gmail.com>
2021-11-10 04:53:53 -0800

    Create GLOBAL VARIABLES.c

---
+++ GLOBAL VARIABLES.c
@@ -0,0 +1,17 @@
+#include <stdio.h>//preprocessor command
+
+// global variable
+int a = 10,b=30;
+
+void main()
+
+{
+    int sum=a+b;
+    printf("sum=%d",sum);
+}
+
+void product()
+{
+     int product=a*b;
+     printf("product=%d",product);
+}
